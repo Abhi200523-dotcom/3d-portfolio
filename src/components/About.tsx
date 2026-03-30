8ef1855af6fbeae360a98caac6ea0b7ebce28d23
Abhi Chand <abhisekhchand23@gmail.com>
2026-03-31 01:53:31 +1300

    Fourth Commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,10 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Passionate about coding, collaborating, and creating impactful solutions, I am always eager to learn and grow in the world of technology. I enjoy building applications and solving problems. I thrive in collaborative environments, where I can contribute my skills while continuously learning from others. Whether it’s developing full-stack applications, designing responsive user interfaces, or working with data, I am always excited to take on new challenges and push my limits!
+          Passionate about coding, collaborating, and creating impactful solutions,I am always eager to learn and grow in the world of technology. I enjoy building applications and solving problems.
+        </p>
+        <p className="para">
+          I thrive in collaborative environments, where I contribute my skills while continuously learning from others. Whether it’s developing full-stack applications, building responsive user interfaces, or working with data, I am always excited to take on new challenges and push my limits.
         </p>
       </div>
     </div>
